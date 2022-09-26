c2a1f2a1974543ce9b9fcda9f874ca7940bd04d4
Andrii-Dmytrenk0 <dmitrenko1567@gmail.com>
2022-09-26 18:42:07 +0200

    app one litl task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const parentEl = document.querySelector('.gallery');
+
+function createElGallery() {
+   const markup = galleryItems.map(
+        (item) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+        </a>
+        </div>`).join("");
+    
+    parentEl.insertAdjacentHTML("beforeend", markup);
+};
+
+createElGallery();
