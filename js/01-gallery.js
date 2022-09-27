a21dd685680ea5391e56647087295223ce219889
Andrii-Dmytrenk0 <dmitrenko1567@gmail.com>
2022-09-27 18:54:58 +0200

    app 1 task finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,3 +24,34 @@ function createElGallery() {
 };
 
 createElGallery();
+
+parentEl.addEventListener('click', imgClick);
+
+function imgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.classList.contains('parentEl')) return;
+
+    const bigImage = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${bigImage}" width="800" height="600">
+`);
+    
+    instance.show()
+
+    document.addEventListener('keydown', event => {
+        if (event.key !== 'Escape') {
+        return
+        };
+
+        instance.close();
+    });
+};
+
+
+
+
+
+
+
